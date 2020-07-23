0754e374da7e00635d934b8a4776d66ca71b3e3b
rayfruit <65891601+rayfruit@users.noreply.github.com>
2020-07-23 10:04:05 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+
+    int n; // angle count
+    int b; // side length
+    int a; // apothem
+    int h; // height
+    
+    printf("Enter your angle count: "); scanf("%d", &n);
+    printf("Enter your side length: "); scanf("%d", &b);
+    printf("Enter your apothem length: "); scanf("%d", &a);
+    printf("Enter your height length: "); scanf("%d", &h);
+    
+    const int vol = ( ( ( ( n * b * a ) / 2 ) * h ) / 3 ) * 2;
+    printf("The volume integer is %d\n", vol);
+    
+    return 0;
+}
 
